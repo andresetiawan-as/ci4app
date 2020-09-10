3ff636d0e5ac4a54a44325bc8ecd512dd1a69f85
Andre Setiawan <andresetiawan.info@gmail.com>
2020-09-10 22:49:06 +0700

    Adding technikparsial for navbar

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -9,12 +9,7 @@
 </head>
 
 <body>
-    <nav class="nav">
-        <a class="nav-link active" href="<?= base_url('/'); ?>">Home</a>
-        <a class="nav-link" href="<?= base_url('/pages/about'); ?>">About</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </nav>
+    <?= $this->include('layout/navbar'); ?>
 
     <?= $this->renderSection('content'); ?>
 
